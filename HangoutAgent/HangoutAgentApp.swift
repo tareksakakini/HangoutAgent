4701b8223040c36a71b5d0a649f730b41ce27d36
Tarek Sakakini <tarek.sakakini@gmail.com>
2025-04-21 11:37:16 -0700

    Initial Commit

---
+++ HangoutAgent/HangoutAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangoutAgentApp.swift
+//  HangoutAgent
+//
+//  Created by Tarek Sakakini on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct HangoutAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
